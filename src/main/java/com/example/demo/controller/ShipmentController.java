1eb77532a502ea1e6986b3d2cef5c31e4993955f
bala-amypo <bala@amypo.in>
2025-12-21 19:05:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/ShipmentController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+
+
+
+import com.example.demo.entity.Shipment;
+
+import com.example.demo.service.ShipmentService;
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+import java.util.List;
+
+
+
+@RestController
+
+@RequestMapping("/shipments")
+
+public class ShipmentController {
+
+
+
+        private final ShipmentService shipmentService;
+
+
+
+            public ShipmentController(ShipmentService shipmentService) {
+
+                        this.shipmentService = shipmentService;
+
+            }
+
+
+
+                @PostMapping
+
+                    public Shipment createShipment(@RequestBody Shipment shipment) {
+
+                                return shipmentService.createShipment(shipment);
+
+                    }
+
+
+
+                        @GetMapping("/{id}")
+                            public Shipment getShipment(@PathVariable Long id) {
+                                        return shipmentService.getShipmentById(id);
+                            }
+                                @GetMapping
+                                    public List<Shipment> getAllShipments() {
+                                                return shipmentService.getAllShipments();
+
+                                    }
+                                    }
