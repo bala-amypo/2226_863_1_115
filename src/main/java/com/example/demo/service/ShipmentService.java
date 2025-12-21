00ca8518743e6f0625d01563cfab70f4bd8b54e4
bala-amypo <bala@amypo.in>
2025-12-21 19:30:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ShipmentService.java
@@ -0,0 +1,28 @@
+le.demo.service;
+
+
+
+import com.example.demo.entity.Shipment;
+
+
+
+import java.util.List;
+
+
+
+public interface ShipmentService {
+
+
+
+        Shipment createShipment(Shipment shipment);
+
+
+
+            Shipment getShipmentById(Long id);
+
+
+
+                List<Shipment> getAllShipments();
+
+                }
+}
