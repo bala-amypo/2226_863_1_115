f5d523b3777b9b08c0e3dd983d1b4fc100c12744
bala-amypo <bala@amypo.in>
2025-12-23 05:53:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AlertController.java
@@ -0,0 +1,15 @@
+  ├─ User.java
+      │           │  ├─ ShipmentRecord.java
+      │           │  ├─ TemperatureSensorLog.java
+      │           │  ├─ TemperatureRule.java
+      │           │  ├─ BreachRecord.java
+      │           │  └─ AlertRecord.java
+
+
+       ├─ repository
+      │           │  ├─ UserRepository.java
+      │           │  ├─ ShipmentRecordRepository.java
+      │           │  ├─ TemperatureSensorLogRepository.java
+      │           │  ├─ TemperatureRuleRepository.java
+      │           │  ├─ BreachRecordRepository.java
+      │           │  └─ AlertRecordRepository.java
