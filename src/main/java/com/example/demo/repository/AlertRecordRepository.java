7a8b5da4efa2ed6bfa5fd85bfe7dc067fe87c0b1
bala-amypo <bala@amypo.in>
2025-12-26 09:53:29 +0000

    User commit

--- src/main/java/com/example/demo/repository/AlertRecordRepository.java
+++ src/main/java/com/example/demo/repository/AlertRecordRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AlertRecordRepository extends JpaRepository<AlertRecord, Long> {
-
     List<AlertRecord> findByShipmentId(Long shipmentId);
 }
--- src/main/java/com/example/demo/repository/TemperatureRuleRepository.java
+++ src/main/java/com/example/demo/repository/TemperatureRuleRepository.java
@@ -6,10 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface TemperatureRuleRepository
-        extends JpaRepository<TemperatureRule, Long> {
-
+public interface TemperatureRuleRepository extends JpaRepository<TemperatureRule, Long> {
     List<TemperatureRule> findByActiveTrue();
-
     Optional<TemperatureRule> findByProductType(String productType);
 }
