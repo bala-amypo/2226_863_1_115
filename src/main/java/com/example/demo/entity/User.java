e224a42bbcad3ab53a883fa02a0ad2e78afaa5a9
bala-amypo <bala@amypo.in>
2025-12-26 08:47:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -11,43 +11,16 @@ public class User {
     private Long id;
 
     private String fullName;
-
-    @Column(unique = true, nullable = false)
+    @Id
     private String email;
-
     private String password;
-
-    private String role;
-
-    public User() {}
-
-    public User(Long id, String fullName, String email, String password, String role) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    @PrePersist
-    public void setDefaultRole() {
-        if (this.role == null) {
-            this.role = "MONITOR";
-        }
-    }
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
     public String getFullName() { return fullName; }
     public void setFullName(String fullName) { this.fullName = fullName; }
-
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
 }
