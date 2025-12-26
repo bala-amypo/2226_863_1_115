97009fc381e9e8760fb17af3fd4948238b51f42a
bala-amypo <bala@amypo.in>
2025-12-26 09:35:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ShipmentRequestDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+public class ShipmentRequestDTO {
+
+    private String shipmentCode;
+    private String origin;
+    private String destination;
+
+    public String getShipmentCode() { return shipmentCode; }
+    public void setShipmentCode(String shipmentCode) { this.shipmentCode = shipmentCode; }
+
+    public String getOrigin() { return origin; }
+    public void setOrigin(String origin) { this.origin = origin; }
+
+    public String getDestination() { return destination; }
+    public void setDestination(String destination) { this.destination = destination; }
+}
